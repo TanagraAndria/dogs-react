53b37be1d77b2e1377e9acab9ba08b8a77c449aa
Tânagra Andria <design.andria@gmail.com>
2021-07-26 19:20:34 -0300

    Components

---
+++ dogs/src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../footer/Footer.module.css'
+
+export const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            Footer
+        </div>
+    )
+}
